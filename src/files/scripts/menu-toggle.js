0dcf6f150d52902d197333479fe7bc72f964f776
Ángel González <aglezabad@gmail.com>
2016-11-20 02:30:11 +0100

    Finished responsive main-menu

---
+++ src/files/scripts/menu-toggle.js
@@ -0,0 +1,41 @@
+function activateMenuToggle (window, document) {
+  var menu = document.querySelector('#main-menu');
+  var WINDOW_CHANGE_EVENT = ('onorientationchange' in window)?
+        'onorientationchange' : 'resize';
+
+  function toggleHorizontal() {
+    [].forEach.call(
+      document.querySelector('#main-menu').querySelectorAll('.can-transform'),
+      function (elem) {
+        elem.classList.toggle('pure-menu-horizontal');
+      }
+    );
+  }
+
+  function toggleMenu() {
+    // set timeout so that the panel has a chance to roll up
+    // before the menu switches states
+    if (menu.classList.contains('open')) {
+        setTimeout(toggleHorizontal, 500);
+    }
+    else {
+        toggleHorizontal();
+    }
+    menu.classList.toggle('open');
+    document.getElementById('toggle').classList.toggle('toggled');
+  }
+
+  function closeMenu() {
+    if (menu.classList.contains('open')) {
+      toggleMenu();
+    }
+  }
+
+  document.querySelector('#toggle').addEventListener('click', function (event) {
+    toggleMenu();
+  });
+
+  window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+}
+
+activateMenuToggle(this, this.document);
