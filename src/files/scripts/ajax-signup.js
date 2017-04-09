683cf483a1102a421fb7b5cc5e3732bc938c19e3
Ángel González <aglezabad@gmail.com>
2017-04-09 03:15:51 +0200

    Signup page

---
+++ src/files/scripts/ajax-signup.js
@@ -0,0 +1,7 @@
+function startJSSignup () {
+  isLogged(function (result) {
+    //Get page content
+    var destHTMLObj = document.querySelector('.unvx-signup .content');
+    var btnArea = destHTMLObj.querySelector('.unvx-btn-area');
+  });
+}
