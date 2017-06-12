8364e0998f245a133a3854064150e5690a9b3175
Ángel González <aglezabad@gmail.com>
2017-06-12 17:19:48 +0200

    Login dialog rewritten

--- src/files/scripts/ajax-login.js
+++ src/files/scripts/ajax-login.js
@@ -1,57 +1,27 @@
 function loadLoginDialog(dstHTMLObj) {
-  // Message part.
-  msgObj = dstHTMLObj.querySelector('p');
-  msgObj.innerHTML = 'Elija cómo entrar en UnivUnix:';
-
-  // Button area part.
-  var githubBtn = document.createElement('a');
-  var iGithubBtn = document.createElement('i');
-  var spanGithubBtn = document.createElement('span');
-  iGithubBtn.classList.add('fa', 'fa-fw', 'fa-github');
-  iGithubBtn.setAttribute('aria-hidden', 'true');
-  spanGithubBtn.innerHTML = 'Github';
-  githubBtn.appendChild(iGithubBtn);
-  githubBtn.appendChild(spanGithubBtn);
-  githubBtn.setAttribute('href', location.origin + '/auth/github');
-  githubBtn.classList.add('pure-button', 'btn-github');
-
-  var googleBtn = document.createElement('a');
-  var iGoogleBtn = document.createElement('i');
-  var spanGoogleBtn = document.createElement('span');
-  iGoogleBtn.classList.add('fa', 'fa-fw', 'fa-google');
-  iGoogleBtn.setAttribute('aria-hidden', 'true');
-  spanGoogleBtn.innerHTML = 'Google';
-  googleBtn.appendChild(iGoogleBtn);
-  googleBtn.appendChild(spanGoogleBtn);
-  googleBtn.setAttribute('href', location.origin + '/auth/google');
-  googleBtn.classList.add('pure-button', 'btn-google');
-
-  btnArea.appendChild(githubBtn);
-  btnArea.appendChild(googleBtn);
+  var msgObj = dstHTMLObj.querySelector('p');
+  var btnArea = dstHTMLObj.querySelector('.unvx-btn-area');
+  msgObj.innerHTML = 'Elija cómo entrar en UnivUnix: ';
+  btnArea.innerHTML = '<a class="pure-button btn-github" href="' +
+    location.origin + '/auth/github">' +
+    '<i class="fa fa-fw fa-github" aria-hidden="true"></i>' +
+    '<span>Github</span></a>' +
+    '<a class="pure-button btn-google" href="' +
+    location.origin + '/auth/google">' +
+    '<i class="fa fa-fw fa-google" aria-hidden="true"></i>' +
+    '<span>Google</span></a>';
 }
 
 function loadLoggedDialog(dstHTMLObj) {
-  // Message part.
-  msgObj = dstHTMLObj.querySelector('p');
+  var msgObj = dstHTMLObj.querySelector('p');
+  var btnArea = dstHTMLObj.querySelector('.unvx-btn-area');
   msgObj.innerHTML = 'Sesión ya iniciada. ¿Quiere cerrarla?';
-
-  // Button area part.
-  var backBtn = document.createElement('a');
-  var spanBackBtn = document.createElement('span');
-  spanBackBtn.innerHTML = 'Volver';
-  backBtn.appendChild(spanBackBtn);
-  backBtn.setAttribute('href', location.origin + '/');
-  backBtn.classList.add('pure-button', 'pure-button-primary');
-
-  var logoutBtn = document.createElement('a');
-  var spanLogoutBtn = document.createElement('span');
-  spanLogoutBtn.innerHTML = 'Cerrar sesión';
-  logoutBtn.appendChild(spanLogoutBtn);
-  logoutBtn.setAttribute('href', location.origin + '/logout');
-  logoutBtn.classList.add('pure-button');
-
-  btnArea.appendChild(backBtn);
-  btnArea.appendChild(logoutBtn);
+  btnArea.innerHTML = '<a class="pure-button pure-button-primary" href="' +
+    location.origin + '/">' +
+    '<span>Volver</span></a>' +
+    '<a class="pure-button" href="' +
+    location.origin + '/logout">' +
+    '<span>Cerrar sesión</span></a>';
 }
 
 function startJSLogin () {
