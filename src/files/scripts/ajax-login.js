3e57d6f662a861819dc3a5b0084d323fb282aeaf
Ángel González <aglezabad@gmail.com>
2017-09-11 17:16:01 +0200

    Merge of two ajax javascript files

--- src/files/scripts/ajax-login.js
+++ src/files/scripts/ajax-login.js
@@ -1,27 +1,19 @@
 function loadLoginDialog(dstHTMLObj) {
-  var msgObj = dstHTMLObj.querySelector('p');
-  var btnArea = dstHTMLObj.querySelector('.unvx-btn-area');
-  msgObj.innerHTML = 'Elija cómo entrar en UnivUnix: ';
-  btnArea.innerHTML = '<a class="pure-button btn-github" href="' +
-    location.origin + '/auth/github">' +
-    '<i class="fa fa-fw fa-github" aria-hidden="true"></i>' +
-    '<span>Github</span></a>' +
-    '<a class="pure-button btn-google" href="' +
-    location.origin + '/auth/google">' +
-    '<i class="fa fa-fw fa-google" aria-hidden="true"></i>' +
-    '<span>Google</span></a>';
+  var req = new XMLHttpRequest();
+  req.addEventListener('load', function () {
+    dstHTMLObj.innerHTML = req.responseText;
+  });
+  req.open('GET', location.origin + '/ajax-partials/login-form.html');
+  req.send();
 }
 
 function loadLoggedDialog(dstHTMLObj) {
-  var msgObj = dstHTMLObj.querySelector('p');
-  var btnArea = dstHTMLObj.querySelector('.unvx-btn-area');
-  msgObj.innerHTML = 'Sesión ya iniciada. ¿Quiere cerrarla?';
-  btnArea.innerHTML = '<a class="pure-button pure-button-primary" href="' +
-    location.origin + '/">' +
-    '<span>Volver</span></a>' +
-    '<a class="pure-button pure-button-primary" href="' +
-    location.origin + '/logout">' +
-    '<span>Cerrar sesión</span></a>';
+  var req = new XMLHttpRequest();
+  req.addEventListener('load', function () {
+    dstHTMLObj.innerHTML = req.responseText;
+  });
+  req.open('GET', location.origin + '/ajax-partials/logged-form.html');
+  req.send();
 }
 
 function startJSLogin () {
--- src/files/scripts/ajax-main.js
+++ src/files/scripts/ajax-main.js
@@ -12,11 +12,41 @@ function getLoggedData (callback) {
   req.send();
 }
 
-function startJSCommon () {
+function startJS () {
+  // Common call for all pages.
+  var bLazy = new Blazy({
+    offset: 250,
+    src: 'lazy-src'
+  });
+  var links = document.querySelector('link');
+  var fontAwesome = document.createElement('link');
+  var fontLato = document.createElement('link');
   var selector = document.querySelector('#unvx-user span');
+  
+  fontAwesome.rel = 'stylesheet';
+  fontAwesome.href = location.origin + '/styles/font-awesome-min.css';
+  fontAwesome.type = 'text/css';
+  links.parentNode.appendChild(fontAwesome);
+
+  fontLato.rel = 'stylesheet';
+  fontLato.href = 'https://fonts.googleapis.com/css?family=Lato:300,400,700';
+  fontLato.type = 'text/css';
+  links.parentNode.appendChild(fontLato);
+
   getLoggedData (function (loggedUser, exists) {
     if (exists) {
       selector.innerHTML = loggedUser.username;
     }
   });
+  // Optional calls for specific pages.
+  if (typeof startJSLogin === 'function') {
+    startJSLogin();
+  }
+  if (typeof startJSSignup === 'function') {
+    startJSSignup();
+  }
 }
+
+window.onload = function () {
+  startJS();
+};
